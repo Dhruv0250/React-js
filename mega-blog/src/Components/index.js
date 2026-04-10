243e2599f7b2302372ef40066057bac73905385e
Dhruv0250 <ds00000777@gmail.com>
2026-04-10 06:59:04 +0530

    posts

--- mega-blog/src/Components/index.js
+++ mega-blog/src/Components/index.js
@@ -4,11 +4,15 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import Logoutbtn from "./Header/Logoutbtn";     
 import RTE from "./RTE";
+import Button from "./button";
+import Input from "./input";
+import Select from "./select";
 import Signup from "../pages/Signup";
  import Login from "../pages/Login";
  import AddPost from "../pages/AddPost";
  import PostForm from "./PostForm";
  import PostCard from "./PostCard"
+ import AuthLayout from "./AuthLayout"
 export {
     header,
     Footer,
@@ -16,9 +20,14 @@ export {
     Logo,
     Logoutbtn,
     RTE,
+    Button,
+    Input,
+    Select,
     Signup,
     Login,
     AddPost,
     PostForm,
-    PostCard
+    PostCard,
+    AuthLayout
+    
 }
--- mega-blog/src/pages/AllPost.jsx
+++ mega-blog/src/pages/AllPost.jsx
@@ -1,17 +1,25 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import appwriteService from "../appwrite/config";
-import { Container,PostCard } from '../Components';
+import { Container, PostCard } from "../Components";
 
 function AllPost() {
-    const [posts,setPosts]=useState([])
-    useEffect(()=>{
-    appwriteService.getPosts([]).then((posts)=> setPosts(posts))
-    },[])
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    appwriteService.getPosts([]).then((posts) => setPosts(posts));
+  }, []);
   return (
-    <div>
-    
+    <div className="w-full py-8">
+      <Container>
+        <div>
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default AllPost
+export default AllPost;
